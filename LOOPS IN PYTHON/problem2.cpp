4dd4705ea8b9208356953f1056eecd26d2db7269
Bikash61 <bashyalb96@gmail.com>
2024-03-04 10:15:47 +0545

    Commiting updates

---
+++ LOOPS IN PYTHON/problem2.cpp
@@ -0,0 +1,16 @@
+// Print the sum of first natural number, where n is the input 
+#include <iostream>
+using namespace std; 
+int main(){
+    int i = 1; 
+    int sum = 0;
+    int  n ; 
+    cout << "Enter the number of sum you want "<<endl;
+    cin >> n ;
+
+    while ( n >=i){
+        sum = sum + i;
+        i ++ ;
+    }
+    cout << sum ; 
+}
