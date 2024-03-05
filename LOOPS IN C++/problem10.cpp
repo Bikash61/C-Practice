f0460ce634abc175977179c63db136d116c6a8b1
Bikash61 <bashyalb96@gmail.com>
2024-03-06 00:06:34 +0545

    Commiting updates

---
+++ LOOPS IN C++/problem10.cpp
@@ -0,0 +1,10 @@
+//Print all the number from 1 to 100 that are divisible by 3
+#include <iostream>
+using namespace std;
+int main(){
+    for( int i = 1; i<=100; i++){
+        if ( i%3 ==0){
+            cout <<i<<endl;
+        }
+    }
+}
