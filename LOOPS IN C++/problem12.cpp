a09de760e18205fb59d3bb23e658ee4ad400e973
Bikash61 <bashyalb96@gmail.com>
2024-03-06 00:30:31 +0545

    Commiting AP problem

---
+++ LOOPS IN C++/problem12.cpp
@@ -0,0 +1,12 @@
+//Display the A.P. 1,3,5,9,5 to n terms 
+#include <iostream>
+using namespace std;
+int main(){
+    int n,d;
+    cout <<"Enter the tern n";
+    cin>> n;
+    for( int i = 1;  i <=n; i++ ){
+       cout << ((2*i)-1)<<endl;
+
+    }
+}
