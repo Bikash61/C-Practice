e776e0ec4b5231f673a77d41ed7138b926591931
Bikash61 <bashyalb96@gmail.com>
2024-03-09 01:20:58 +0545

    sum of givem number

--- LOOPS IN C++/sumOfGivenDigit.cpp
+++ LOOPS IN C++/sumOfGivenDigit.cpp
@@ -1,21 +1,14 @@
 #include <iostream>
-
-int main() {
-    int num, sum = 0, digit;
-
-    // Input the number
-    std::cout << "Enter a number: ";
-    std::cin >> num;
-
-    // Calculate sum of digits
-    while (num != 0) {
-        digit = num % 10; // Get the last digit
-        sum += digit;     // Add the digit to the sum
-        num /= 10;        // Remove the last digit
+using namespace std; 
+int main(){
+    int num , digit , count = 0;
+    cout <<"Enter the number"<<endl;
+    cin>>num;
+    while( num !=0){
+        digit = num % 10 ;
+        count += digit;
+        num /=10;
     }
-
-    // Output the sum
-    std::cout << "Sum of digits: " << sum << std::endl;
-
-    return 0;
+    cout << count <<endl;
+    return 0 ;
 }
