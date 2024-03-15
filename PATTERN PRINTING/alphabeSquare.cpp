952bdbf3890503aef33bd35bfa41e5f97aee83b3
Bikash61 <bashyalb96@gmail.com>
2024-03-15 18:58:30 +0545

    Commiting updates

---
+++ PATTERN PRINTING/alphabeSquare.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    for(int i = 0; i <=4; i++){
+        for(char ch ='A';  ch<='D' ;  ch++ ){
+            cout<< ch;
+        }
+        cout<<endl;
+    }
+}
