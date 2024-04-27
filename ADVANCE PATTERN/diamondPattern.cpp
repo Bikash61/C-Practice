94416571f2d2fa5067bd6ae9130ad3d147ddb9ae
Bikash61 <bashyalb96@gmail.com>
2024-04-27 21:01:59 +0545

    Commiting updates

--- ADVANCE PATTERN/diamondPattern.cpp
+++ ADVANCE PATTERN/diamondPattern.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int r ; 
-    cin>>r;
+int main()
+{
+    int r;
+    cin >> r;
 
-    int spaces = r-1;
+    int spaces = r - 1;
 
-    for (int i = 0; i<r; i++){
-        for(int j = 0; j<spaces ; j++){
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < spaces; j++)
+        {
             cout << " ";
         }
-        for(int j = 0; j<i+1; j++){
-            cout<< "*";
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << "*";
         }
-      
-        cout<< endl;
-        spaces --;
 
+        cout << endl;
+        spaces--;
     }
 }
