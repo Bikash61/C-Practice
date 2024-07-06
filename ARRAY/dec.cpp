d5e5fc8ba34f9d390e800097b1bd6ebc7932b6a3
Bikash61 <bashyalb96@gmail.com>
2024-07-06 18:56:24 +0545

    Commiting updates

---
+++ ARRAY/dec.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[10] ;
+    cout<< endl<<"The value at 4 index is "<<arr[2]<<endl;
+    cout<<"Everything is fine";
+
+    return 0;
+}
