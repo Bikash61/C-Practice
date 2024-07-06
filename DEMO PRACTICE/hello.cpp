c0284970bb57959dbd29dbd78850e2c83c9949b5
Bikash61 <bashyalb96@gmail.com>
2024-07-06 18:17:19 +0545

    Commiting Updates

---
+++ DEMO PRACTICE/hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<< "How are you?:";
+    int health;
+    cin>> health;
+    
+    if (health = 1){
+        cout<<"Health is good";
+        }else{
+            cout<<"Health is bad";
+        }
+    
+}
