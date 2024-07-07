1a2286d8606b2e3e0e1c379b91abcfb8cf6e90ba
Bikash61 <bashyalb96@gmail.com>
2024-07-07 18:34:15 +0545

    Updating

---
+++ ARRAY/differentTypesOfArray.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char ch[5] ={ 'a', 'b','e'};
+    cout<<ch[2];
+    cout<<endl;
+    for (int i =0; i<3; i++){
+        cout<<ch[i]<<" ";
+    }
+}
