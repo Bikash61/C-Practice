ff2dc74f61b8e30071350a1b6c5bd2403e737bd6
Bikash61 <bashyalb96@gmail.com>
2024-07-07 19:24:47 +0545

    Commiting updates

---
+++ ARRAY/maxMinInArray.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int getmax(int num[], int n){
+    int max = INT32_MIN;
+
+    for (int i =0; i<n ; i++){
+        if (num[i]>max){
+            max = num[i];
+        }
+    }
+    return max;
+
+}
+int getmin(int num[], int n){
+    int min = INT32_MAX;
+
+    for (int i =0; i<n ; i++){
+        if (num[i]<min){
+            min = num[i];
+        }
+    }
+    return min;
+
+}
+
+int main(){
+    int size;
+    cout<<"Give us the size of an array"<<endl;
+    cin>>size;
+    int num[100];
+
+    //Taking Input 
+
+    for(int i =0; i<size ; i++){
+        cin>>num[i];
+    }
+    cout<<"Maximum value is "<<getmax(num, size)<<endl;
+    cout<<"Minimum value is "<<getmin(num, size)<<endl;
+
+    return 0;
+}
