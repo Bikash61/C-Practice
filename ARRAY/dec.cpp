40cbde9b75959394f41f727f7380ca44c01c274d
Bikash61 <bashyalb96@gmail.com>
2024-07-07 09:26:03 +0545

    Commiting updates

--- ARRAY/dec.cpp
+++ ARRAY/dec.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 using namespace std;
 int main(){
+    //declaring an array
     int arr[10] ;
+
+    //Accessing an array
     cout<< endl<<"The value at 4 index is "<<arr[2]<<endl;
     cout<<"Everything is fine";
 
