8e1efc85e01c4b64bb1e8105e2a4cb5d7b335a83
Bikash61 <bashyalb96@gmail.com>
2024-07-07 17:04:59 +0545

    Commiting updates

--- ARRAY/dec.cpp
+++ ARRAY/dec.cpp
@@ -2,11 +2,28 @@
 using namespace std;
 int main(){
     //declaring an array
-    int arr[10] ;
+    int number[10] ;
 
     //Accessing an array
-    cout<< endl<<"The value at 4 index is "<<arr[2]<<endl;
-    cout<<"Everything is fine";
+    cout<<"The value at 4 index is "<<number[2]<<endl;
+
+    cout<<"The value at 20 index is "<<number[20]<<endl;
+
+    cout<<"The value at 788 index is "<<number[788]<<endl;
+
+
+
+    cout<<"Everything is fine"<<endl;
+
+    int secondArray[3] = {1,3,4};
+
+    cout<<"The value of 2Nd index is "<<secondArray[2]<<endl;
+
+    int thirdArray[8] ={2,4,5,6,4,3};
+    cout<<"The value of 7th index is "<<thirdArray[7]<<endl;
+
+
+
 
     return 0;
 }
