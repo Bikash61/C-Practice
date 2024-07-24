80b85f0754b59e84d259829a6ccd8aa70aadc9ad
Bikash61 <bashyalb96@gmail.com>
2024-07-24 22:11:02 +0545

    Updates

--- LEET CODE PROBLEM 1/complimrntBaseTenInteger.cpp
+++ LEET CODE PROBLEM 1/complimrntBaseTenInteger.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 using namespace std;
 int main(){
-    
+    int x;
+    cout<< "Write the number you want print"<<endl;
+    cin >> x;
+    cout << "The number ypu want to print is : "<< x;
+
+
 }
