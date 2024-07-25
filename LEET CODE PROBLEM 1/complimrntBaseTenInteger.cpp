e32080793f2d3e68acd032030a46788a8e1dc32a
Bikash61 <bashyalb96@gmail.com>
2024-07-25 22:50:24 +0545

    updates

--- LEET CODE PROBLEM 1/complimrntBaseTenInteger.cpp
+++ LEET CODE PROBLEM 1/complimrntBaseTenInteger.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int x;
-    cout<< "Write the number you want print"<<endl;
-    cin >> x;
-    cout << "The number ypu want to print is : "<< x;
+    int day;
+    cout<< "Enter the day you want to print?"<<endl;
+    cin>>day;
 
+    switch(day){
+        case 1:
+        cout << "This is a Sunday"<<endl;
+        break;
+        case 2:
+        cout << "This is a Monday"<<endl;
+        break;
+        case 3:
+        cout << "This is a Tuesday"<<endl;
+        break;
+        case 4:
+        cout << "This is a Wednesday"<<endl;
+        break;
+        case 5:
+        cout << "This is a Thursday"<<endl;
+        break;
+        case 6:
+        cout << "This is a Friday"<<endl;
+        break;
+        case 7:
+        cout << "This is a Saturday"<<endl;
+        break;
+        default:
+        cout << "Invalid day"<<endl;
 
+    }
 }
