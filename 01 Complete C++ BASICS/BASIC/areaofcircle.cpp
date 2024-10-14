609887ac7c207d53f39a9f49c51722520da9ecd2
Bikash61 <bashyalb96@gmail.com>
2024-10-14 23:58:46 +0530

    Updating the content

---
+++ 01 Complete C++ BASICS/BASIC/areaofcircle.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float r;
+    cout << "Enter the raduis of a circle " <<endl;
+    cin >> r;
+
+    float area = 3.14 *( r*r);
+    cout << "The area of a circle is: " << area <<endl; 
+
+
+
+
+}
