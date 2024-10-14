723c683678d3e1f2f60d685f1de8ebe4ed7a7d3c
Bikash61 <bashyalb96@gmail.com>
2024-10-14 22:12:01 +0530

    updating the content

--- 01 OOPS ONE SHOT/01 BASICS/main.cpp
+++ 01 OOPS ONE SHOT/01 BASICS/main.cpp
@@ -1,33 +1,42 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
 class Teacher
 {
 public:
+
+    //Properties
     string name;
     string dept;
     string subject;
     int salary;
-
     // Methods/ Member Functions
-
     void ChangeDept(string newDept)
     {
         dept = newDept;
     }
 };
 
+
+class Account{
+
+    private:
+        string password;
+        double bankbalance; // Implementing Data Hiding 
+
+    public:
+        string name;
+        string accountID;
+};
+
+
 int main()
 {
-
     Teacher t1;
     t1.name = "Bikash Bashyal";
     t1.dept = "Computer Science";
     t1.subject = "DSA";
     t1.salary = 50000;
-
     cout << t1.name << endl;
-
     return 0;
 }
